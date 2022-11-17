fd1bd80225c8c294ff3d9cc1a0713f2fcb452bac
bookpark <bkbkgg@gmail.com>
2022-11-17 21:29:54 +0900

    TodoItem 작성

--- src/components/js/TodoItemList.js
+++ src/components/js/TodoItemList.js
@@ -1,10 +1,13 @@
 import { Component } from 'react';
+import TodoItem from './TodoItem';
 
 class TodoItemList extends Component {
     render() {
         return (
             <div>
-                TodoItem 자리
+                <TodoItem content='TodoItem1' />
+                <TodoItem content='TodoItem2' />
+                <TodoItem content='TodoItem3' />
             </div>
         )
     }
