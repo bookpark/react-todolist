4d82e85c0db3eee15748f4a14edc25c1f734e0d5
bookpark <bkbkgg@gmail.com>
2022-11-17 20:39:46 +0900

    Form 작성 및 적용

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import TodoListTemplate from './components/js/TodoListTemplate';
+import Form from './components/js/Form';
 
 function App() {
   return (
     <div>
-      <TodoListTemplate>
+      <TodoListTemplate form={<Form />}>
         안녕하세요?
       </TodoListTemplate>
     </div>
---
+++ src/components/js/Form.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../css/Form.css';
+
+function Form({ value, onChange, onCreate, onKeyPress }) {
+    return (
+        <>
+            <div className='form'>
+                <input value={value} placeholder='오늘 할 일을 입력하세요.' onChange={onChange}
+                    onKeyPress={onKeyPress} />
+                <div className='create-button' onClick={onCreate}>추가</div>
+            </div>
+        </>
+    )
+}
+
+export default Form;
