a8849a8a5696c4317487a7f570d0f4c0c2688f9d
bookpark <bkbkgg@gmail.com>
2022-11-18 09:22:07 +0900

    class형 변경 constructor 추가 및 필요 메서드 바인드

--- src/App.js
+++ src/App.js
@@ -1,17 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 import TodoListTemplate from './components/js/TodoListTemplate';
 import Form from './components/js/Form';
 import TodoItemList from './components/js/TodoItemList';
 
-function App() {
-  return (
-    <div>
-      <TodoListTemplate form={<Form />}>
-        <TodoItemList />
-      </TodoListTemplate>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.id = 2;
+    this.state = {
+      input: "",
+      todos: [
+        { id: 0, content: '리액트 공부', isComplete: false },
+        { id: 1, content: '스프링 공부', isComplete: true },
+      ]
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleCreate = this.handleCreate.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      input: event.target.value
+    });
+  }
+
+  handleCreate() {
+    const { input, todos } = this.state;
+    if (input === "") {
+      alert('오늘 할 일을 입력해주세요!');
+      return;
+    }
+    this.setState({
+      input: '',
+      todos: todos.concat({
+        id: this.id++,
+        content: input,
+        isComplete: false
+      })
+    });
+  }
+
+  handleKeyPress(event) {
+    if (event.key === 'Enter') {
+      this.handleCreate();
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <TodoListTemplate form={(
+          <Form
+            value={this.state.input}
+            onChannge={this.handleChange}
+            onCreate={this.handleCreate}
+            onKeyPress={this.handleKeyPress} />
+        )}>
+          <TodoItemList />
+        </TodoListTemplate>
+      </div >
+    );
+  }
 }
 
 export default App;
